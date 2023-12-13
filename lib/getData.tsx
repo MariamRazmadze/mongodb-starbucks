4831860d576ca809a4122ba6458bdd0cca4a565e
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2023-12-13 10:42:06 +0400

    ajsdgh

--- lib/getData.tsx
+++ lib/getData.tsx
@@ -1,8 +1,8 @@
-const API_URL = "https://starbucksapi.pythonanywhere.com";
-const BASE_URL = "http://localhost:3000";
+// const API_URL = "https://starbucksapi.pythonanywhere.com";
+// const BASE_URL = "http://localhost:3000";
 
 export async function getCoffees() {
-  const res = await fetch(`/api/coffee`, {
+  const res = await fetch(`${BASE_URL}/api/coffee`, {
     next: { revalidate: 60 },
   });
   if (!res.ok) throw Error("failed to fetch data");
@@ -10,31 +10,31 @@ export async function getCoffees() {
 }
 
 export async function getHomepageData() {
-  const res = await fetch(`/api/homepage`);
+  const res = await fetch(`${BASE_URL}/api/homepage`);
   if (!res.ok) throw Error("Failed to fetch data");
   return res.json();
 }
 
 export async function getFooterData() {
-  const res = await fetch(`/api/footer`);
+  const res = await fetch(`${BASE_URL}/api/footer`);
   if (!res.ok) throw Error("failed to fetch data");
   return res.json();
 }
 
 export async function getRewardsData() {
-  const res = await fetch(`/api/rewards`);
+  const res = await fetch(`${BASE_URL}/api/rewards`);
   if (!res.ok) throw Error("failed to fetch data");
   return res.json();
 }
 
 export async function fetchCities() {
-  const res = await fetch(`/api/cities`);
+  const res = await fetch(`${BASE_URL}/api/cities`);
   if (!res.ok) throw Error("failed to fetch data");
   return res.json();
 }
 
 export async function fetchCity(id: string) {
-  const res = await fetch(`/cities/${id}`);
+  const res = await fetch(`${BASE_URL}/cities/${id}`);
   if (!res.ok) throw Error("Failed getting city");
 
   const data = await res.json();
@@ -65,14 +65,14 @@ export async function fetchAddress() {
 }
 
 export async function getOrders() {
-  const res = await fetch(`/api/order`);
+  const res = await fetch(`${BASE_URL}/api/order`);
   if (!res.ok) throw Error("failed to fetch data");
   const json = await res.json();
   return json.data;
 }
 
 export async function getOrder(id: string) {
-  const res = await fetch(`/api/order/${id}`);
+  const res = await fetch(`${BASE_URL}/api/order/${id}`);
   if (!res.ok) return undefined;
   const json = await res.json();
   return json;
